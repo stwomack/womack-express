422f95fce373fc82c24ae69f6d78857e5159e0b7
Steve Womack <stwomack@gmail.com>
2016-04-09 17:43:01 -0500

    Enhances script

--- bg.sh
+++ bg.sh
@@ -1,8 +1,10 @@
+sed -i '' 's/GREEN/BLUE/g' views/index.jade
 cf push Blue -n womack-express
 # Change HTML to say Green
+sed -i '' 's/BLUE/GREEN/g' views/index.jade
 cf push Green -n womack-express-green
 # Apps both have routes 
 # This gives us time to run automated & manual tests against Green until we're confident. 
-cf map-route Green cfapps.pez.pivotal.io -n womack-express
+cf map-route Green cfapps.io -n womack-express
 # This allows us to do A/B Testing
-cf unmap-route Blue cfapps.pez.pivotal.io -n womack-express
+# cf unmap-route Blue cfapps.io -n womack-express
