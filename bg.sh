2559293b57ea76deb3ebae43c3b8abf2477d1f5b
Steve Womack <stwomack@gmail.com>
2016-06-26 12:59:30 -0500

    Cuts green instances to 3, not 6

--- bg.sh
+++ bg.sh
@@ -1,10 +1,14 @@
+# Make sure app says BLUE
 sed -i '' 's/GREEN/BLUE/g' views/index.jade
-cf push Blue -n womack-express
+# Push Blue app
+cf push Blue -n womack-express -b nodejs_buildpack
 # Change HTML to say Green
 sed -i '' 's/BLUE/GREEN/g' views/index.jade
-cf push Green -n womack-express-green
+# Push Green app
+cf push Green -n womack-express-green -b nodejs_buildpack
 # Apps both have routes 
 # This gives us time to run automated & manual tests against Green until we're confident. 
-cf map-route Green cfapps.io -n womack-express
+cf map-route Green pcf1.fe.gopivotal.com -n womack-express
+cf scale green -i 3
 # This allows us to do A/B Testing
-# cf unmap-route Blue cfapps.io -n womack-express
+# cf unmap-route Blue pcf1.fe.gopivotal -n womack-express
